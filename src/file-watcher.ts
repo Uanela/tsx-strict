af0ad1451a0f73a1ce619bbc6501f97cc19bfd06
uanela <uanelaluiswayne@gmail.com>
2026-04-04 22:37:39 +0200

    feat(tsx-strict): support config file for watch options

--- src/file-watcher.ts
+++ src/file-watcher.ts
@@ -1,41 +1,51 @@
 import chokidar, { FSWatcher } from "chokidar";
 import sheu from "./sheu";
 import { getStatus } from ".";
+import { TsxStrictConfig, WatchOptions } from "./config";
 
 let fileWatcher: FSWatcher | null = null;
 
-export function setupFileWatcher(restartTsx: () => void) {
+export function setupFileWatcher(
+  restartTsx: () => void,
+  config: Partial<TsxStrictConfig> = {}
+) {
   if (fileWatcher) {
     fileWatcher.close();
     fileWatcher = null;
   }
 
-  fileWatcher = chokidar.watch([process.cwd()], {
-    ignored: [/node_modules/, /.build/, /dist/],
+  const watchOptions =
+    typeof config.watch === "object"
+      ? config.watch
+      : ({} as Partial<WatchOptions>);
+
+  fileWatcher = chokidar.watch(watchOptions.include ?? [process.cwd()], {
+    ignored: [/node_modules/, /.build/, /dist/, ...(watchOptions.ignore ?? [])],
     ignoreInitial: true,
     persistent: true,
   });
 
   let isRestarting = false;
+
   fileWatcher.on("all", (event, path) => {
     if (
       event === "ready" ||
       isRestarting ||
-      (!/\.(ts|js|jsx|tsx|mts|cts|mjs|cjs)$/.test(path) &&
+      (!(watchOptions.extensions
+        ? new RegExp(`\\.(${watchOptions.extensions.join("|")})$`).test(path)
+        : /\.(ts|js|jsx|tsx|mts|cts|mjs|cjs)$/.test(path)) &&
         !path.includes(".env"))
     )
       return;
-    isRestarting = true;
 
+    isRestarting = true;
     path = path.replace(process.cwd(), "");
     path = path.startsWith("/")
       ? path.replace("/", "")
       : path.startsWith("\\")
         ? path.replace("\\", "")
         : path;
-
     const status = getStatus();
-
     if (!status?.hasTsErrors)
       sheu.info(`Restarting because of file changes: ${path}`, {
         timestamp: true,
